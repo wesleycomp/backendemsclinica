103831181a81580b95d83b5410d8ab1fdf219836
Wesley <wesleycomp@gmail.com>
2022-06-23 16:19:36 -0300

    atualizacao inserir avatar do usuario

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,11 +1,18 @@
 import { celebrate, Joi, Segments } from 'celebrate';
 import { Router} from 'express'
+import multer from 'multer'
+import uploadConfig from '@config/upload'
 import UsersController from '../controllers/UsersController';
 import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
+
 usersRouter.get('/', isAuthenticated, usersController.index);
 
 usersRouter.post(
@@ -19,5 +26,12 @@ usersRouter.post(
     }),
     usersController.create,
 );
+
+    usersRouter.patch(
+        '/avatar',
+        isAuthenticated,
+        upload.single('avatar'),
+        usersAvatarController.update,
+    )
 
 export default usersRouter;
