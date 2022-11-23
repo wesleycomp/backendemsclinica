21ee870ea0a8dc345c700652894480dd867c2a77
Wesley <wesleycomp@gmail.com>
2022-11-23 17:32:58 -0300

    assinando xml

--- src/modules/aso/services/CreateXMLService.ts
+++ src/modules/aso/services/CreateXMLService.ts
@@ -17,9 +17,19 @@ var FileKeyInfo = require('xml-crypto').FileKeyInfo
 
 interface IExameAso{
     aso_id: string;
+
 }
 
 class CreateXMLService{
+
+
+    //   public async creaMyKeyInfote(request: Request, response: Response): Promise<Response>{
+
+    //                return "<X509Data></ X509Data>"
+    //   }
+
+
+
 
     public async execute({aso_id}: IExameAso): Promise<Aso | undefined>{
         //instaciou o repositorio para ter acesso aos metodos(save, delete... etc)
@@ -109,92 +119,44 @@ class CreateXMLService{
 //const xmlaso = await fs.readFileSync('./xml/arquivoTesteXml.xml', "utf8");
 
 
-var myKey = fs.readFile("./certificado/key.pem", "utf8") //.replace("-----BEGIN ENCRYPTED PRIVATE KEY-----", "").replace("-----END ENCRYPTED PRIVATE KEY-----", "").trim();
-console.log("My key is: ", myKey);
+// var myKey = fs.readFile("./certificado/key.pem", "utf8") //.replace("-----BEGIN ENCRYPTED PRIVATE KEY-----", "").replace("-----END ENCRYPTED PRIVATE KEY-----", "").trim();
+// console.log("My key is: ", myKey);
 
 
-// Convert string to buffer
-////const xmlEms = Buffer.from(asoXml);
 
-
- //const publicKey =  fs.readFileSync("./certificado/certificado.pem", { encoding: "utf8" });
-
-//console.log(publicKey)
-
-var certtifi="-----BEGIN CERTIFICATE-----\n"+
-"MIIHLDCCBRSgAwIBAgIIFSoiEQdSP3QwDQYJKoZIhvcNAQELBQAwWTELMAkGA1UE"+
-"BhMCQlIxEzARBgNVBAoTCklDUC1CcmFzaWwxFTATBgNVBAsTDEFDIFNPTFVUSSB2"+
-"NTEeMBwGA1UEAxMVQUMgQ0VSVElGSUNBIE1JTkFTIHY1MB4XDTIyMTEwODEyMzQw"+
-"MFoXDTIzMTEwODEyMzQwMFowgdYxCzAJBgNVBAYTAkJSMRMwEQYDVQQKEwpJQ1At"+
-"QnJhc2lsMQswCQYDVQQIEwJUTzEPMA0GA1UEBxMGUGFsbWFzMR4wHAYDVQQLExVB"+
-"QyBDRVJUSUZJQ0EgTUlOQVMgdjUxFzAVBgNVBAsTDjM0NzQ2MDY2MDAwMTQ3MRkw"+
-"FwYDVQQLExBWaWRlb2NvbmZlcmVuY2lhMRowGAYDVQQLExFDZXJ0aWZpY2FkbyBQ"+
-"SiBBMTEkMCIGA1UEAxMbVklWSUFOIFBBUlJBOjMwODU4NjQ2MDAwMTc1MIIBIjAN"+
-"BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAi5U27/ZZG0WacbkqIDZ5brLc5DAg"+
-"pqLEMZHyf++Zcnfu86hYXkJsKfkYTdwea9dNYHzdAd4RARhSGgRxvA+oZHJ7Ahun"+
-"w3rTEPZX2HjBR8+lTL7RXJKGNZGoSKGJpuJPv7sNfqshXDKo8uPj2HOYbAprpqcZ"+
-"lufm3ORZUpCfZyJV75f1MFz1UPWuGTlbdyBAEXFOhEY833dBuMMEzOIMlGi96cjz"+
-"o+mZFoZ25+oc+0Jcq9M3XvyVxZc4B+rYLSW6zixBMzifz1N77e8Zt7+4WG7QNAsL"+
-"LTOrDJQbS+USYR45TDFxI8foiZByIEyOdcTNKdHp2n9JD7BxEe+06yo57QIDAQAB"+
-"o4ICeDCCAnQwHwYDVR0jBBgwFoAUP9NcqRlN14gWLZgMrwre4U8kFrAwWQYIKwYB"+
-"BQUHAQEETTBLMEkGCCsGAQUFBzAChj1odHRwOi8vY2NkLmFjc29sdXRpLmNvbS5i"+
-"ci9sY3IvYWMtY2VydGlmaWNhbWluYXMtc21pbWUtdjUucDdiMIGqBgNVHREEgaIw"+
-"gZ+BFnZpdmlwYXJyYW1lZEBnbWFpbC5jb22gFwYFYEwBAwKgDhMMVklWSUFOIFBB"+
-"UlJBoBkGBWBMAQMDoBATDjMwODU4NjQ2MDAwMTc1oDgGBWBMAQMEoC8TLTE1MDIx"+
-"OTg5MDE0NjE5MjAxMDgwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMKAXBgVgTAED"+
-"B6AOEwwwMDAwMDAwMDAwMDAwYgYDVR0gBFswWTBXBgZgTAECAWAwTTBLBggrBgEF"+
-"BQcCARY/aHR0cDovL2NjZC5hY3NvbHV0aS5jb20uYnIvZG9jcy9kcGMtYWMtY2Vy"+
-"dGlmaWNhbWluYXMtc21pbWUucGRmMB0GA1UdJQQWMBQGCCsGAQUFBwMCBggrBgEF"+
-"BQcDBDCBlgYDVR0fBIGOMIGLMEOgQaA/hj1odHRwOi8vY2NkLmFjc29sdXRpLmNv"+
-"bS5ici9sY3IvYWMtY2VydGlmaWNhbWluYXMtc21pbWUtdjUuY3JsMESgQqBAhj5o"+
-"dHRwOi8vY2NkMi5hY3NvbHV0aS5jb20uYnIvbGNyL2FjLWNlcnRpZmljYW1pbmFz"+
-"LXNtaW1lLXY1LmNybDAdBgNVHQ4EFgQUc5xBOCEoV0XE1mkY5dz804TrVTwwDgYD"+
-"VR0PAQH/BAQDAgXgMA0GCSqGSIb3DQEBCwUAA4ICAQBJ3S2kdjnlmQQybLipEe1a"+
-"Ged7QPBoGBTbmo3VDB2yhBpod1fPcMivtjOx3LB1kLrL4Wp1K0RI9iOBl1VVr4J+"+
-"Jm2usU2sc0rzMvOIEsxewCFjfGhME2aTf7f5kusLkLT2aVVSryGGBfJkvTMBBC6Q"+
-"FvQ6eIH+xzoPydX7Wn2rmLORQJXrfuUHTs9aRLovIbJ4gsN5Cc6vigohfAcoPCpk"+
-"YuiEQUFimtgcrCUymu36PLWXD+/yTZQunZDUuY6tQcxbw2hx7A3CJS8rjOakmuUA"+
-"12is0rpebRNegNUov4mAGDF3aj1x+83s/r+uxwD3XO25YtqhBY8Gx2gZpKmlQtyf"+
-"OjKfO/pTUIJfwGcROjWnDaY88zcbrt5s4w1scMZuRh0U4HmmfAtYd9RQFFlJ+e3b"+
-"uREZFYZPEmBhW8gjGE6qph+nskN9XVS09eLK5Onue3lYLK1TQjaRIHh0QZS7+4k/"+
-"idh+zSdVx8MaL46aMl1pcR/zcWMyXWTjCu65FfSpR8TBaHQobsNQJMMaKnUyp1V5"+
-"hrgMwnmErzcV2ds8Wcc0Lo6xBHThixPG+29H8xI7kCNa7EmNVFeHcdOhyu8yy6j+"+
-"n2DGKtsNfO5R0d7aTntPL0pQMZJX77a4kLXrvoGq/VPyA0zGUmgTJRlohjpM6sMC"+
-"VRZauBfLGR00CULsdq5qiQ=="+
-"\n-----END CERTIFICATE-----"
-
-
-//console.log(certtifi)
-
-var xml = "<library>" +
-	            "<book>" +
-	              "<name>Harry Potter</name>" +
-	            "</book>" +
-	          "</library>"
 
               try{
 
-            console.log('passou aki 1' )
-
+            console.log('passou aki 1')
             var sig = new SignedXml();
 
-                sig.addReference("//*[local-name(.)='book']")
-
+                sig.addReference("//*[local-name(.)='evtMonit']")
                 console.log('passou aki 2' )
-                sig.signingKey=certtifi //fs.readFileSync('./certificado/certificadoteste.pem') //fs.readFileSync("./certificado/certificado.pem", { encoding: "utf8" })
-                //sig.signatureAlgorithm = 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256'; // set signing algorithme
-               // console.log('passou aki 3'+sig.signingKey)
-                console.log(sig.signingKey)
-                sig.computeSignature(xml)
-                console.log('passou aki 4' )
+
+                sig.signingKey= fs.readFileSync('./certificado/certificado.key') //fs.readFileSync("./certificado/certificado.pem", { encoding: "utf8" })
+                sig.keyInfoProvider = new FileKeyInfo("./certificado/certificado2.pem")
+                sig.canonicalizationAlgorithm= 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315'
+                sig.signatureAlgorithm = 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256'; // set signing algorithme
+                sig.addReference("/*", ['http://www.w3.org/2000/09/xmldsig#enveloped-signature', 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315'],'http://www.w3.org/2001/04/xmlenc#sha256');
+
+               // sig.HashAlgorithms='http://www.w3.org/2001/04/xmlenc#sha256'
+              // console.log('passou aki 3'+sig.signingKey)N
+               // console.log(sig.signingKey)
+              //***********************   TESTE PERSONALIZAR ALGORITIMO   ********************************* */
+              // SignedXml.CanonicalizationAlgorithms["http://MyTransformation"] =['http://www.w3.org/2000/09/xmldsig#enveloped-signature', 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315']
+              // SignedXml.HashAlgorithms["http://myDigestAlgorithm"] = 'http://www.w3.org/2001/04/xmlenc#sha256'
+
+              //  sig.addReference("/*", ["http://MyTransformation"], "http://myDigestAlgorithm")
+
+                sig.computeSignature(xmlEsocial)
+                console.log('passou aki 4')
                 //fs.writeFileSync("./xml/arquivoTesteXml.xml", sig.getSignedXml())
-                fs.writeFile('./xml/arquivoTesteXml.xml', sig.getSignedXml());
-               console.log('passou aki final' )
+                fs.writeFileSync('./xml/arquivoTesteXml.xml', sig.getSignedXml());
+                console.log('passou aki final')
 
                } catch (e) {
 
             console.log(e);
-
         }
 
 
