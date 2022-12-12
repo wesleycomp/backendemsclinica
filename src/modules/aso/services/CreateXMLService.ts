a735babc5368fb5ce3a60d57049bd55d16d3c022
wesleycomp <weslleycomp@gmail.com>
2022-12-11 21:39:19 -0300

    atualizacao xml 33

--- src/modules/aso/services/CreateXMLService.ts
+++ src/modules/aso/services/CreateXMLService.ts
@@ -151,7 +151,7 @@ class CreateXMLService {
                 console.log(statusCode);
             })();
 
-            console.log('passou aki final envio lote xml... atualizaei pull 22')
+            console.log('passou aki final envio lote xml... atualizaei pull 33')
 
         } catch (e) {
             console.log(e);
