3257a423e34a1fd3ccef63d2760be29cf553351e
Wesley <wesleycomp@gmail.com>
2022-12-15 14:39:24 -0300

    atualizacao

--- src/modules/aso/services/CreateAsosService.ts
+++ src/modules/aso/services/CreateAsosService.ts
@@ -3,7 +3,6 @@ import Aso from "../typeorm/entities/Aso";
 import { AsosRepository } from "../typeorm/repositories/AsosRepository";
 import AppError from '@shared/errors/AppError';
 
-
 interface IAso{
     dataemissaoaso:Date;
     paciente_id: string;
@@ -17,19 +16,17 @@ interface IAso{
     transmissaoesocial: boolean;
     ativo: boolean;
 }
+
 class CreateAsoService{
 
     public async execute({dataemissaoaso,paciente_id,empresa_id,funcao_id,tipoaso_id,tipopagamento_id,medico_id,resultado,temexames,transmissaoesocial,ativo}: IAso): Promise<Aso>{
 
         //instaciou o repositorio para ter acesso aos metodos(save, delete... etc)
     const asoRepository = getCustomRepository(AsosRepository);
-
       //const exameExists= await asoRepository.findByName(name)
-
       //  if(exameExists){
       //      throw new AppError('Exame ja existente')
       //  }
-
         const aso = asoRepository.create({
             dataemissaoaso,
             paciente_id,
