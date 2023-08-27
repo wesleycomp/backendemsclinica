e133c85f37920fbd3e8399ef7d310b06801dbfcb
wesleycomp <weslleycomp@gmail.com>
2023-08-27 18:36:52 -0300

    aditando usuario

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -55,7 +55,7 @@ export default class UsersController{
                     perfil
             })
 
-            return response.status(204).json();
+            return response.json(resetPassword);
         }
 
 }
